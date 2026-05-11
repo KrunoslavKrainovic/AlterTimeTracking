f12d72ed0cc94130c4bb15eae618d9d90291b461
Krunoslav Krainovic <58593872+KrunoslavKrainovic@users.noreply.github.com>
2026-05-12 01:36:16 +0200

    Revert "fixed when there is no more buff to remove icon/text"
    This reverts commit d06fede6670e2aad4a0d7e0199bc81d3b1fc77a5.

--- core.lua
+++ core.lua
@@ -12,9 +12,8 @@ Core.alterTimeStartTime = nil
 Core.alterTimeDuration = 10
 
 -- Alter Time spell IDs
-local ALTER_TIME_SPELL_ID = 342247  -- Cast spell
-local ALTER_TIME_BUFF_ID = 342246   -- Actual buff
-local ALTER_TIME_RECALL_ID = 371817 -- Recall spell
+local ALTER_TIME_SPELL_ID = 342247
+local ALTER_TIME_RECALL_ID = 371817
 local debugMode = false
 
 -- Slash command to toggle debug mode
@@ -47,7 +46,6 @@ function Core:Init()
     frame:RegisterEvent("PLAYER_ENTERING_WORLD")
     frame:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
     frame:RegisterEvent("ADDON_LOADED")
-    frame:RegisterUnitEvent("UNIT_AURA", "player")
     
     frame:SetScript("OnEvent", function(self, event, ...)
         if event == "ADDON_LOADED" then
@@ -59,27 +57,17 @@ function Core:Init()
         elseif event == "UNIT_SPELLCAST_SUCCEEDED" then
             local unit, castGUID, spellID = ...
             if unit == "player" then
-                if spellID == ALTER_TIME_SPELL_ID then
-                    -- Alter Time was cast (to cancel) - clear everything
+                if spellID == ALTER_TIME_RECALL_ID then
+                    if Core.savedHealth == nil then
+                        Core.savedHealth = UnitHealth("player")
+                        Core.savedHealthPercent = UnitHealthPercent("player", true, CurveConstants.ScaleTo100) or 100
+                        Core.alterTimeStartTime = GetTime()
+                    end
+                elseif spellID == ALTER_TIME_SPELL_ID then
                     Core.savedHealth = nil
                     Core.savedHealthPercent = nil
                     Core.alterTimeStartTime = nil
                 end
-            end
-        elseif event == "UNIT_AURA" then
-            -- Check if Alter Time buff exists (342246)
-            local hasAlterTimeBuff = C_UnitAuras.GetPlayerAuraBySpellID(ALTER_TIME_BUFF_ID)
-            
-            if hasAlterTimeBuff and not Core.savedHealth then
-                -- Buff just appeared - save health and start timer
-                Core.savedHealth = UnitHealth("player")
-                Core.savedHealthPercent = UnitHealthPercent("player", true, CurveConstants.ScaleTo100) or 100
-                Core.alterTimeStartTime = GetTime()
-            elseif not hasAlterTimeBuff and Core.savedHealth then
-                -- Buff disappeared (dispelled, spell stolen, canceled, or expired) - clear everything
-                Core.savedHealth = nil
-                Core.savedHealthPercent = nil
-                Core.alterTimeStartTime = nil
             end
         end
         Core:UpdateDisplay()
